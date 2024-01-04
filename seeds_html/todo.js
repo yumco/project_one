eb855a9de6ef15d35d98896fd1604b75ddc1ea96
yumco <pmwoj.022@gmail.com>
2024-01-04 13:41:00 +0900

    2024/01/04: todo list 엔터 동시에 삭제 오류 고침 , 나의 페이지에 뒤로가기 아이콘 추가

--- seeds_html/todo.js
+++ seeds_html/todo.js
@@ -1,18 +1,14 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = document.querySelector("#todo-form input");
+const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
 const toDos = [];
 
-
 function saveToDos(){
-    localStorage.setItem(TODOS_KEY , JSON.stringify(toDos));
+    localStorage.setItem("todos", JSON.stringify(toDos));
 }
-
-
-
 
 function deleteToDo(event){
    const li = event.target.parentElement;
@@ -21,38 +17,30 @@ function deleteToDo(event){
 
 
 
-function painToDo(newTodo) {
+function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
+    span.innerText = newTodo;
     const button = document.createElement("button");
-    button.innerText = "❌";
+    button.innerText="❌";
     button.addEventListener("click", deleteToDo)
     li.appendChild(span);
     li.appendChild(button);
-    span.innerText = newTodo;
     toDoList.appendChild(li);
+    
+
 }
 
 
 
 function handleToDoSubmit(event) {
-     event.preventDefault();
-     const newTodo = toDoInput.value;
-     console.log(toDoInput.value);
-    toDoInput.value="";
-    toDos.push(newTodo);
-    console.log(newTodo, toDoInput.value);
-    painToDo(newTodo);
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value ="";
+    paintToDo(newTodo);
     saveToDos();
+    
+   
 }
 
-toDoForm.addEventListener("submit", handleToDoSubmit);
-
-
-
-const savedToDos = localStorage.getItem("TODOS_KEY");
-
-
-if(saveToDos){
-    const parsedToDos= JSON.parse(saveToDos);
-}
+toDoForm.addEventListener("submit", handleToDoSubmit) ;
